729ce46503b27549eacae9bfdbf737435fd62cb7
kotaro takahashi <kotaro.t@k4h4shi.com>
2017-07-07 19:57:17 +0900

    initial commit

---
+++ src/main/java/com/k4h4shi/todoRestApiServer/TodoRestApiServerApplication.java
@@ -0,0 +1,12 @@
+package com.k4h4shi.todoRestApiServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRestApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoRestApiServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/k4h4shi/todoRestApiServer/TodoRestApiServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.k4h4shi.todoRestApiServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodoRestApiServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
